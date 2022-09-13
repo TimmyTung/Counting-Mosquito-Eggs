a86c7c715d98b983c00b4550bb162e2be08a78b5
AllTimeGamer3 <43817633+AllTimeGamer3@users.noreply.github.com>
2022-09-12 19:18:12 -0700

    Added the number part to show how many eggs are in each cluster.

--- Counting Eggs With AI/semester2.py
+++ Counting Eggs With AI/semester2.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2
 import math
 import os
+font = cv2.FONT_HERSHEY_TRIPLEX
 
-FILENAME = "./stackPhotos/stack5.jpg"
+FILENAME = "./home/woowat/Documents/Capstone Project/Testing image stuff/stack5.jpg"
 
 # SETUP - CREATE BLACK MASK AND FIND CONTOURS
 lower_black = np.array([0, 0, 0], dtype="uint16")
@@ -24,19 +25,24 @@ dilated = cv2.dilate(canny, (1, 1), iterations=1)
 (contours, heirarchy) = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 rgb = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 cv2.drawContours(rgb, contours, -1, (0, 255, 0), 2)
-cv2.imshow("final", rgb)
-cv2.waitKey(0)
+
 
 # NEXT STEP - CALCULATE AREA
-
 # assuming 4350 is the area of one egg, and count is number of eggs
 count = 0;
 for i in contours:
     area = cv2.contourArea(i)
-    if(area > 4000):
+    x,y,w,h = cv2.boundingRect(i)
+    
+    if(area > 4350):
         count += math.floor(area/4350)
+        num = math.floor(area/4350)
+        cv2.putText(rgb,str(num),(math.floor(x+w/2), math.floor(y+h/2)),font, 2, (255, 0, 0))
     elif(area > 3000):
         count += 1
-
-
+        num = 1
+        cv2.putText(rgb,str(num),(math.floor(x+w/2), math.floor(y+h/2)),font, 2, (255, 0, 0))
+        
 print("Number of eggs: " + str(count));
+cv2.imwrite("final.jpg", rgb)
+cv2.waitKey(0)
