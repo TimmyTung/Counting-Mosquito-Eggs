84510241c4c73ac70f0163c0d9e68778d7c0a6f6
AllTimeGamer3 <43817633+AllTimeGamer3@users.noreply.github.com>
2022-11-12 21:26:05 -0700

    Added Different Color Bounding Boxes
    Also removed dead comments and cleaned up the code a little bit.

--- Semester_2_Counting_Eggs_With_AI/finalAlgoWithBoundingBoxes.py
+++ Semester_2_Counting_Eggs_With_AI/finalAlgoWithBoundingBoxes.py
@@ -4,6 +4,7 @@ import math
 import os
 import sys
 import math
+import random
 
 # function that rejects outliers from a numpy array and returns that array without outliers
 # obtained from stack overflow: https://stackoverflow.com/questions/11686720/is-there-a-numpy-builtin-to-reject-outliers-from-a-list
@@ -12,6 +13,12 @@ def reject_outliers(data, m = 2.):
     mdev = np.median(d)
     s = d/mdev if mdev else 0.
     return data[s<m]
+
+#Generates a random color for the bounding boxes and text color
+def random_color():
+    rgbl=[255,0,0]
+    random.shuffle(rgbl)
+    return tuple(rgbl)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 thickness = 7
@@ -56,11 +63,8 @@ for arg in range(imgStart, len(sys.argv)):
     dilated = cv2.dilate(canny, (1, 1), iterations=1)
     (contours, heirarchy) = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     rgb = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-    #cv2.drawContours(rgb, contours, -1, (0, 255, 0), 2)
-    #cv2.imshow("final", rgb)
-    #cv2.waitKey(0)
     if len(contours) == 0:
-         print("No eggs found, please run another image.");
+         print("No eggs found, please run another image.")
          exit(0)
 
     if str(sys.argv[1]) == "--sheet": #SHEET IMAGE ALGO
@@ -82,38 +86,37 @@ for arg in range(imgStart, len(sys.argv)):
 
         # NEXT STEP - CALCULATE COUNT
         # assuming EGG_AREA is the area of one egg, and count is number of eggs
-        count = 0;
-        offset = 0.18;
+        count = 0
+        offset = 0.18
         if EGG_AREA > 500 and EGG_AREA < 600:
-            offset = .33;
+            offset = .33
         elif EGG_AREA > 400 and EGG_AREA < 450:
-            offset = .27;
+            offset = .27
         elif EGG_AREA > 450 and EGG_AREA < 500:
-            offset = .18;
+            offset = .18
         elif EGG_AREA < 400 and EGG_AREA > 300:
-            offset = .335;
+            offset = .335
         elif EGG_AREA < 300 and EGG_AREA > 200:
-            offset = .4     #untested
+            offset = .4     
         elif EGG_AREA < 200 and EGG_AREA > 100:
-            offset = .49    #untested
+            offset = .49    
         elif EGG_AREA < 100:
             offset = .578
         elif EGG_AREA >= 1000:
             offset = .17
-        #print(EGG_AREA)
-        #print(offset)
         for i in contours:
             area = cv2.contourArea(i)
             x,y,w,h = cv2.boundingRect(i)
+            color = random_color()
             if(area > EGG_AREA+(EGG_AREA*.4)):
                 count += math.floor(area/(EGG_AREA-(EGG_AREA*offset)))
                 if displayBoundingBoxes == True:
                     num = math.floor(area/(EGG_AREA-(EGG_AREA*offset)))
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                    cv2.putText(img,str(num),(math.floor(x+w/2)-4*thickness, math.floor(y+h/2)+4*thickness),font, 4, color, thickness)
+                    cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                    cv2.putText(img,str(num),(math.floor(x+w/2)-4*thickness, math.floor(y+h/2)+4*thickness),font, 2, color, thickness)
             elif(area > EGG_AREA-(EGG_AREA*.4)):
                 if displayBoundingBoxes == True:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
                 count += 1
 
 
@@ -121,23 +124,22 @@ for arg in range(imgStart, len(sys.argv)):
     elif str(sys.argv[1]) == "--stack":
          # NEXT STEP - CALCULATE AREA
         # assuming 4350 is the area of one egg, and count is number of eggs
-        count = 0;
+        count = 0
         for i in contours:
             area = cv2.contourArea(i)
             x,y,w,h = cv2.boundingRect(i)
             if(area > 4350):
+                color = random_color()
                 count += math.floor(area/4350)
                 if displayBoundingBoxes == True:
                     num = math.floor(area/4350)
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
                     cv2.putText(img,str(num),(math.floor(x+w/2-thickness)-4*thickness, math.floor(y+h/2-thickness)+4*thickness),font, 4, color, thickness)
                 elif(area > 3000):
                     if displayBoundingBoxes == True:
-                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
                     count += 1
         
-    print("Number of eggs in file " + str(sys.argv[arg]) + ": " + str(count));
+    print("Number of eggs in file " + str(sys.argv[arg]) + ": " + str(count))
     if displayBoundingBoxes == True:
         cv2.imwrite(str(sys.argv[arg]).split('.')[0] + "WithBoundingBoxes.jpg", img)
-        
-
